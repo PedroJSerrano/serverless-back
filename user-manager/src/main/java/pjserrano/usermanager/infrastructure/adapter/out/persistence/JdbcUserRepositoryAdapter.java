98442058beff15c5756714314340d827ca50aff8
USERSAD\pserranc <pserranc@emeal.nttdata.com>
2025-08-12 16:36:06 +0200

    Eliminacion de test de integracion y produccion (futuro desarrollo con test de integracion con H2 para BBDD y mock para servicios AWS) Simplificacion de workflow

--- user-manager/src/main/java/pjserrano/usermanager/infrastructure/adapter/out/persistence/JdbcUserRepositoryAdapter.java
+++
@@ -1,6 +0,0 @@
-package pjserrano.usermanager.infrastructure.adapter.out.persistence;
-
-import pjserrano.usermanager.application.port.out.UserRepositoryPort;
-
-public class JdbcUserRepositoryAdapter implements UserRepositoryPort {
-}
