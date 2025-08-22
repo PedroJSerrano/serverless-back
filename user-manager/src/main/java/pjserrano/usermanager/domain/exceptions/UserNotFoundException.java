ee2a89ab9b719f59a9809440dee9c55e5ba8b702
USERSAD\pserranc <pserranc@emeal.nttdata.com>
2025-08-22 18:21:33 +0200

    feat: implementacion user-manager

--- user-manager/src/main/java/pjserrano/usermanager/domain/exceptions/UserNotFoundException.java
+++ user-manager/src/main/java/pjserrano/usermanager/domain/exceptions/UserNotFoundException.java
@@ -1,7 +1,7 @@
 package pjserrano.usermanager.domain.exceptions;
 
 public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
+    public UserNotFoundException() {
+        super("User not found");
     }
 }
