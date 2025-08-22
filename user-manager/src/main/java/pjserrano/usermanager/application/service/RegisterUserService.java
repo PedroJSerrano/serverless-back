572a8ad503097c9b653be67aea7f4e8798b27c3d
USERSAD\pserranc <pserranc@emeal.nttdata.com>
2025-08-22 18:08:08 +0200

    feat: initial commit
    Commit inicial de user-manager a branch

--- user-manager/src/main/java/pjserrano/usermanager/application/service/RegisterUserService.java
+++ user-manager/src/main/java/pjserrano/usermanager/application/service/RegisterUserService.java
@@ -1,6 +1,4 @@
 package pjserrano.usermanager.application.service;
 
-import pjserrano.usermanager.application.port.in.RegisterUserUseCase;
-
-public class RegisterUserService implements RegisterUserUseCase {
+public class RegisterUserService {
 }
