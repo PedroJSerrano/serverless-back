68f1ecd100e73aa6aad1ea9c8a24bafba24ba0a8
USERSAD\pserranc <pserranc@emeal.nttdata.com>
2025-08-23 21:09:42 +0200

    tests: correcciones de tests

--- auth-manager/src/test/java/pjserrano/authmanager/config/AwsConfigUnitTest.java
+++ auth-manager/src/test/java/pjserrano/authmanager/config/AwsConfigUnitTest.java
@@ -3,8 +3,13 @@ package pjserrano.authmanager.config;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.ContextConfiguration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ssm.SsmClient;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -15,6 +20,18 @@ class AwsConfigUnitTest {
 
     @Autowired
     private ApplicationContext context;
+
+    @TestConfiguration // <-- Esta anotación es la clave
+    static class TestAwsConfig {
+        @Bean
+        public SsmClient ssmClient() {
+            // Devuelve un cliente mockeado que no necesita credenciales reales
+            return SsmClient.builder()
+                    .region(Region.EU_WEST_1)
+                    .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create("test", "test")))
+                    .build();
+        }
+    }
 
     @Test
     void testSsmClientBeanCreation() {
