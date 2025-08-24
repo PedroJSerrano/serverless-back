21bc6aa00e143bd5652bc2b63fe04d74605d5cbc
USERSAD\pserranc <pserranc@emeal.nttdata.com>
2025-08-24 12:33:19 +0200

    ci: definicion de evento pull_request en workflow de ci auth-manager

--- auth-manager/src/main/java/pjserrano/authmanager/domain/exceptions/InvalidCredentialsException.java
+++ auth-manager/src/main/java/pjserrano/authmanager/domain/exceptions/InvalidCredentialsException.java
@@ -2,6 +2,7 @@ package pjserrano.authmanager.domain.exceptions;
 
 public class InvalidCredentialsException extends RuntimeException {
     public static final String ERROR_MESSAGE = "Las credenciales indicadas no son válidas";
+
     public InvalidCredentialsException() {
         super(ERROR_MESSAGE);
     }
