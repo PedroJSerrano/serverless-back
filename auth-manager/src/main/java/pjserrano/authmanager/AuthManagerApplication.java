4d82522529e3a46814f51d0d5d43b52a5caca068
USERSAD\pserranc <pserranc@emeal.nttdata.com>
2025-08-24 21:08:02 +0200

    feat: add aws common infraestructure (#76)
    Desarrollada libreria con la infraestructura comun de aws para las funciones lambda

--- auth-manager/src/main/java/pjserrano/authmanager/AuthManagerApplication.java
+++ auth-manager/src/main/java/pjserrano/authmanager/AuthManagerApplication.java
@@ -2,8 +2,10 @@ package pjserrano.authmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"pjserrano.authmanager", "pjserrano.awscommon"})
 public class AuthManagerApplication {
 
     public static void main(String[] args) {
