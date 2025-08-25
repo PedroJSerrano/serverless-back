8c3d745eab3a48b25308b7b36e559d0d358fff75
USERSAD\pserranc <pserranc@emeal.nttdata.com>
2025-08-25 12:00:41 +0200

    ci: modificacion de workflow, prueba

--- auth-manager/src/test/java/pjserrano/authmanager/domain/exceptions/InvalidCredentialsExceptionUnitTest.java
+++ auth-manager/src/test/java/pjserrano/authmanager/domain/exceptions/InvalidCredentialsExceptionUnitTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-class InvalidCredentialsExceptionTest {
+class InvalidCredentialsExceptionUnitTest {
 
     @Test
     void testNoArgsConstructor() {
