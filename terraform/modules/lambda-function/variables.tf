a828cc65aa10934dcdc4d713f24fdba3107b59fd
USERSAD\pserranc <pserranc@emeal.nttdata.com>
2025-08-31 21:12:10 +0200

    chore: implementacion de terraform

---
+++ terraform/modules/lambda-function/variables.tf
@@ -0,0 +1,69 @@
+variable "function_name" {
+  description = "Name of the Lambda function"
+  type        = string
+}
+
+variable "handler" {
+  description = "Lambda function handler"
+  type        = string
+}
+
+variable "runtime" {
+  description = "Lambda runtime"
+  type        = string
+}
+
+variable "timeout" {
+  description = "Lambda timeout in seconds"
+  type        = number
+  default     = 30
+}
+
+variable "memory_size" {
+  description = "Lambda memory size in MB"
+  type        = number
+  default     = 512
+}
+
+variable "zip_file" {
+  description = "Path to the Lambda deployment package"
+  type        = string
+}
+
+variable "environment_variables" {
+  description = "Environment variables for the Lambda function"
+  type        = map(string)
+  default     = {}
+}
+
+variable "custom_policies" {
+  description = "Custom IAM policies for the Lambda function"
+  type        = list(any)
+  default     = []
+}
+
+variable "api_id" {
+  description = "API Gateway ID"
+  type        = string
+}
+
+variable "api_execution_arn" {
+  description = "API Gateway execution ARN"
+  type        = string
+}
+
+variable "route_path" {
+  description = "API Gateway route path"
+  type        = string
+}
+
+variable "http_method" {
+  description = "HTTP method for the route"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
