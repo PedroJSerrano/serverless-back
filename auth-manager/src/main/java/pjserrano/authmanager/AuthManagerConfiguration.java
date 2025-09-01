ff974ea721bb1ee9130721792a6718f3f8683ea1
USERSAD\pserranc <pserranc@emeal.nttdata.com>
2025-09-01 13:23:17 +0200

    chore: implementacion de terraform

--- auth-manager/src/main/java/pjserrano/authmanager/AuthManagerConfiguration.java
+++ auth-manager/src/main/java/pjserrano/authmanager/AuthManagerConfiguration.java
@@ -2,10 +2,15 @@ package pjserrano.authmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.function.context.config.ContextFunctionCatalogAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"pjserrano.authmanager", "pjserrano.awscommon"})
+@Import(ContextFunctionCatalogAutoConfiguration.class)
 public class AuthManagerConfiguration {
-    public static void main(String[] args) { SpringApplication.run(AuthManagerConfiguration.class, args); }
+    public static void main(String[] args) {
+        SpringApplication.run(AuthManagerConfiguration.class, args);
+    }
 }
--- user-manager/src/main/java/pjserrano/usermanager/UserManagerFunctionalApplication.java
+++
@@ -1,9 +0,0 @@
-package pjserrano.usermanager;
-
-import org.springframework.cloud.function.context.FunctionalSpringApplication;
-
-public class UserManagerFunctionalApplication extends FunctionalSpringApplication {
-    public static void main(String[] args) {
-        FunctionalSpringApplication.run(UserManagerFunctionalApplication.class, args);
-    }
-}
